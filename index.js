2154c4bc7621fc5e24e2d5c55902fa800717d9c1
michellejanosi <michelle.janosi@gmail.com>
2022-06-26 05:39:08 -0400

    Create list item element

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const inputEl = document.querySelector('#input-el');
 const inputBtn = document.querySelector('#input-btn');
+const ulEl = document.querySelector('#ul-el');
 let myLeads = [];
 
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);
 
   myLeads.forEach(function (lead) {
-    console.log(lead);
+    // ulEl.innerHTML += `<li>${lead}</li>`;
+    const li = document.createElement('li');
+    li.innerText = lead;
+    ulEl.append(li);
   });
   
   inputEl.value = ' ';
