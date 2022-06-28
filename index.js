d802c248a66210e6ec2f18679f99f9471ef378c5
michellejanosi <michelle.janosi@gmail.com>
2022-06-28 05:28:46 -0400

    Set leads in local storage

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let myLeads = [];
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);
   inputEl.value = '';
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
   renderLeads();
 });
 
