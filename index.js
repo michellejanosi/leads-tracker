b354084974853852c987f5e240d8a928041bedea
michellejanosi <michelle.janosi@gmail.com>
2022-07-01 05:55:30 -0400

    Add function to grab active browser link and save to leads

--- index.js
+++ index.js
@@ -11,16 +11,12 @@ if (leadsFromLocalStorage) {
   render(myLeads);
 }
 
-const tabs = {
-  url: 'https://www.linkedin.com/in/per-harald-borgen/'
-};
-
 tabBtn.addEventListener('click', function () {
-  // grab url of current tab (chrome API)
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {}
-  myLeads.push(tabs[0].url);
-  localStorage.setItem('myLeads', JSON.stringify(myLeads));
-  render(myLeads);
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
 deleteBtn.addEventListener('dblclick', function () {
